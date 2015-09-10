940c19b33e985975116d64a3b337a71c0beed36d
j-i-k-o <jiko.rogy@gmail.com>
2015-09-10 22:19:47 +0900

    delete AssimpModel

---
+++ build/gl_debug.d
@@ -0,0 +1,11 @@
+build/gl_debug.o: src/gl_debug.cpp src/../include/gl_base.h \
+  src/../include/gl_helper.h src/../include/gl_debug.h \
+  src/../include/gl_3D.h
+
+src/../include/gl_base.h:
+
+src/../include/gl_helper.h:
+
+src/../include/gl_debug.h:
+
+src/../include/gl_3D.h:
---
+++ build/main.d
@@ -0,0 +1,20 @@
+build/main.o: src/main.cpp src/../include/gl_all.h \
+  src/../include/gl_base.h src/../include/gl_helper.h \
+  src/../include/gl_debug.h src/../include/gl_3D.h \
+  src/../include/gl_main.h src/shader.vert src/shader.frag
+
+src/../include/gl_all.h:
+
+src/../include/gl_base.h:
+
+src/../include/gl_helper.h:
+
+src/../include/gl_debug.h:
+
+src/../include/gl_3D.h:
+
+src/../include/gl_main.h:
+
+src/shader.vert:
+
+src/shader.frag:
